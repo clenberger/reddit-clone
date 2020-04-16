1c9ac4fd4d2c973fff2e2eb94804e29d34e7772a
clenberger <christian.lenberger@gmail.com>
2020-04-15 17:44:19 -0700

    added middleware(handlebars)

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
+    res.send('Hello world');
 });
 
 // Start Server
-
+app.listen(3000, () => {
+    console.log('Reddit listening on port localhost:3000!');
+});
